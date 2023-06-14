073e1e3d16d38678317a7f70a7b8fd8093dff73b
bpkroth <bpkroth@users.noreply.github.com>
2023-06-14 19:38:52 +0000

    Deploying to gh-pages from @ microsoft/MLOS@1e6f5637788fd9db8e141c88cb888243b7f6fa2f 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev1+g07dc5a9',
+    VERSION: '0.1.dev1+g1e6f563',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
