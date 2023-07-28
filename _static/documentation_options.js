4c7319d65f26be25489b64fa24065b26bb46300f
motus <sergiy.matusevych@gmail.com>
2023-07-28 00:46:42 +0000

    Deploying to gh-pages from @ microsoft/MLOS@3b9fd29a4b66f9929282d5368ba86c9e3e638267 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev1+g755950c',
+    VERSION: '0.1.dev1+g3b9fd29',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
