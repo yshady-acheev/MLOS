504be58fad2b6e33cb5166b144bbe2f805a63950
bpkroth <bpkroth@users.noreply.github.com>
2024-07-10 21:12:48 +0000

    Deploying to gh-pages from @ microsoft/MLOS@5a062865bebad22b0da4e433865daed5ff8d8e21 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.5.2.dev46+ga989d895e',
+    VERSION: '0.5.2.dev47+g5a062865b',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
