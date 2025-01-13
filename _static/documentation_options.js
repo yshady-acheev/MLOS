ab9b4220c7e3b91ffbb3082f4c958e5529c542f0
motus <sergiy.matusevych@gmail.com>
2025-01-13 22:03:21 +0000

    Deploying to gh-pages from @ microsoft/MLOS@3c319cd97004a098dfd2e9074922e3242d8022b5 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.6.3.dev9+g440b2378d2',
+    VERSION: '0.6.3.dev11+g3c319cd970',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
